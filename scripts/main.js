4f6a214b676858045275a792db92efc00bcb2717
Muhammad Aji Muharrom <ajim1417@gmail.com>
2019-05-11 15:29:34 +0700

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+const anchors = ["home", "hikmah1", "about", "hikmah2", "aza"]
+
+function handleOnLeave(origin, destination, direction)
+{
+  let divBg = document.getElementById('bg-placeholder')
+  let divBg2 = document.getElementById('bg-placeholder2')
+  if(origin.index < 2 && destination.index >= 2)
+  {
+    divBg.classList.add('bg-two')
+    divBg2.classList.add('bg2-two')
+    // divBg.classList.remove('bg-one')
+  }
+
+  if(origin.index >= 2 && destination.index < 2) {
+    divBg.classList.remove('bg-two')
+    divBg2.classList.remove('bg2-two')
+  }
+}
